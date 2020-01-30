26337d1aab623b29c3e067258d509d90312939e6
Bruno Alberto da Veiga Pinheiro <contato@brunoavpinheiro.com.br>
2020-01-30 16:04:04 -0300

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -70,7 +70,7 @@ function install() {
     
             mkdir -p $OUTPUT_DIR/letsencrypt
             docker pull certbot/certbot
-            docker run -it --rm --name certbot -p 80:80 -v $OUTPUT_DIR/letsencrypt:/etc/letsencrypt/ certbot/certbot \
+            docker run -it --rm --name certbot -p 80:6680 -v $OUTPUT_DIR/letsencrypt:/etc/letsencrypt/ certbot/certbot \
                 certonly --standalone --noninteractive  --agree-tos --preferred-challenges http \
                 --email $EMAIL -d $DOMAIN --logs-dir /etc/letsencrypt/logs
         fi
@@ -117,7 +117,7 @@ function updateLetsEncrypt() {
     if [ -d "${OUTPUT_DIR}/letsencrypt/live" ]
     then
         docker pull certbot/certbot
-        docker run -i --rm --name certbot -p 443:443 -p 80:80 \
+        docker run -i --rm --name certbot -p 443:6443 -p 80:6680 \
             -v $OUTPUT_DIR/letsencrypt:/etc/letsencrypt/ certbot/certbot \
             renew --logs-dir /etc/letsencrypt/logs
     fi
